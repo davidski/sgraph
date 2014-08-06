ef75f3115ffc7a0a429d2acc791b352a02f8b5d9
davidski <davidski@deadheaven.com>
2014-08-06 16:28:49 -0700

    Add comments for what the various elements mean. Rename domain_data to malware_domains.

--- R/sgraph.R
+++ R/sgraph.R
@@ -13,11 +13,19 @@
 #' }
 get_sgraph <- function(ip = NA, domain = NA, auth_token = NA) {
   if (!is.na(ip)) {
+    #returns history of the A records associated with this IP for the past 90 days
     ip_data <- GET(paste0("https://investigate.api.opendns.com/dnsdb/ip/a/", ip, ".json"), add_headers("Authorization"= paste("Bearer", auth_token)))
+    
+    #returns any currently known malware domains on this IP
     domains <- GET(paste0("https://investigate.api.opendns.com/ips/", ip, "/latest_domains"), add_headers("Authorization"= paste("Bearer", auth_token)))
-    sgraph <- list(ip_data=content(ip_data), domain_data=content(domains))
+    sgraph <- list(ip_data=content(ip_data), malware_domains=content(domains))
   } else {
+    #returns status   integer 	The status will be "-1" if the domain is believed to be malicious, "1" if the domain is believed to be benign, "0" if it hasn't been classified yet
+    #security_categories 	array of strings 	The OpenDNS security category or categories that match this domain. If none match, the return will be blank.
+    #content_categories 	array of strings 	The OpenDNS content category or categories that match this domain. If none match, the return will be blank.
     domain_category <- GET(paste0("https://investigate.api.opendns.com/domains/categorization/", domain), add_headers("Authorization"= paste("Bearer", auth_token)))
+    
+    
     domain_name <- GET(paste0("https://investigate.api.opendns.com/security/name/", domain), add_headers("Authorization"= paste("Bearer", auth_token)))
     domain_name <- content(domain_name)
     domain_name <- unlist(strsplit(domain_name, "\n"))
